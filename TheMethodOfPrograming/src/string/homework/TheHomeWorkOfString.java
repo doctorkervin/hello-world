e95e21b88de0e5dfe79cda4e04f0deb19883b684
kervin <doctorkervin@gmail.com>
2020-03-04 22:20:15 +0800

    @dkervin 编程之法----字符串的homework1

--- TheMethodOfPrograming/src/string/homework/TheHomeWorkOfString.java
+++ TheMethodOfPrograming/src/string/homework/TheHomeWorkOfString.java
@@ -8,10 +8,45 @@ public class TheHomeWorkOfString {
     /**
      * 字符串反转
      */
+    private String revertOfString(String str){
+        char[] chars = str.toCharArray();
+        for (int l = 0, r = chars.length - 1; l < r; l++, r--) {
+            Character temp = chars[l];
+            chars[l] = chars[r];
+            chars[r] = temp;
+        }
+        return new String(chars);
+    }
 
     /**
      * 字符串的左右移动
+     * 字符串为*和26个字母随机组成，现在需要将*号左移动，字母向右移动且顺序不变
      */
+    private String moveOfString(String str){
+        char[] chars = str.toCharArray();
+        int t = chars.length-1;
+        for (int i = 0; i < t; i++) {
+            if (chars[i] != '*'){
+                for (int j = t; j > i ; j--) {
+                    if (chars[j] == '*'){
+                        Character temp = chars[j];
+                        chars[j] = chars[i];
+                        chars[i] = temp;
+                        t = j;
+                        //跳出整个循环
+                        break;
+                    }else {
+                        //跳过本次循环，继续下次循环
+                        continue;
+                    }
+                }
+            }else {
+                continue;
+            }
+        }
+        return new String(chars);
+    }
+
 
     /**
      * 字符个数的统计
