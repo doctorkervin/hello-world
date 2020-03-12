3d5911595c543d91264a122cefeb7dcd3c5ee3ef
kervin <doctorkervin@gmail.com>
2020-03-12 22:37:47 +0800

    @dkervin 编程之法----快排实现最小k个数

--- TheMethodOfPrograming/src/array/one/TheSmallKofArray.java
+++ TheMethodOfPrograming/src/array/one/TheSmallKofArray.java
@@ -62,4 +62,53 @@ public class TheSmallKofArray {
         }
         return al;
     }
+
+    //O(n)
+    public ArrayList<Integer> getLeastNumbers(int nums[], int k) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int lens = nums.length;
+        if (nums == null || lens == 0 || k > lens || k <= 0) {
+            return list;
+        }
+        int start = 0;
+        int end = lens - 1;
+        int index = partition(nums, start, end);
+        while (index != k - 1) {
+            if (index > k - 1) {
+                end = index - 1;
+                index = partition(nums, start, end);
+            } else {
+                start = index + 1;
+                index = partition(nums, start, end);
+            }
+        }
+        for (int i = 0; i < k; i++) {
+            list.add(nums[i]);
+        }
+        return list;
+    }
+
+    //快排
+    private int partition(int[] nums, int start, int end) { //快排
+        int privotKey = nums[start];
+        while (start < end) {
+            while (start < end && privotKey <= nums[end]) {
+                --end;
+            }
+            swap(nums, start, end);//交换位置，使大于privotkey的值位于数组右边
+
+            while (start < end && privotKey >= nums[start]) {
+                ++start;
+            }
+            swap(nums, start, end);//交换位置，使小于privotkey的值位于数组左边
+        }
+        return start;
+    }
+
+    //交换数组元素位置
+    private void swap(int[] nums, int start, int end) {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+    }
 }
